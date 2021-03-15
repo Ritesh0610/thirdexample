2ac0ad6628f884d917bd2e9bda8b24bcfab030fa
Ritesh0610 <yadavitin197@gmail.com>
2021-03-15 16:11:30 +0530

    Second Commit

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppMVC.Models
+{
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
