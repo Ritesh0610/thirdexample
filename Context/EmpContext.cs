752552ea2d908f22c29b7f0e991881b961a981a8
Ritesh0610 <yadavitin197@gmail.com>
2021-03-15 16:28:57 +0530

    Third Commit

---
+++ Context/EmpContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAppMVC.Models;
+
+namespace WebAppMVC.Context
+{
+    public class EmpContext:DbContext
+    {
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
